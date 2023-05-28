8e5e13b80b8645980aa16ac3eeb82632fe272cea
Lopuh <1opuhart@gmail.com>
2023-05-28 19:33:14 +0900

    manage volume

--- main.py
+++ main.py
@@ -5,25 +5,35 @@ file = 'music.mp3'
 
 mixer.music.load(file)
 mixer.music.play()
-is_paused = True
+is_paused = False
+volume = mixer.music.get_volume()
 
-def pause_music():
+def pause_music(action):
     global is_paused
-    if is_paused:
+    if is_paused and action=='p':
         mixer.music.unpause()
         is_paused = False
-    else:
+    elif not is_paused and action=='p':
         mixer.music.pause()
         is_paused=True
+
+def manage_volume(action):
+    if action=='-':
+        mixer.music.set_volume(volume-0.9)
+    if action=='+':
+        mixer.music.set_volume(volume+0.9)
+
+def close(action):
+    if action=='e':
+        mixer.music.stop()
+        return True
 
 def start():
     while True:
         action=input()
-        if action=='p':
-            pause_music()
-        if action=='e':
-            mixer.music.stop()
-            break
+        if close(action): break
+        pause_music(action)
+        manage_volume(action)
 
 if '__main__'==__name__:
     start()
