a847c1b2d2fc2b26f0cbfa540781d453dfa682bb
Lopuh <1opuhart@gmail.com>
2023-06-01 13:37:15 +0900

    choose music

--- main.py
+++ main.py
@@ -1,50 +1,77 @@
 from pygame import mixer
-import pygame
-import os
+import pygame, sys, os
 from playlist import playlist
+
 pygame.init()
 mixer.init()
 directory = 'music/'
 
+font = pygame.font.SysFont('Comic Sans', 30)
+white = (255,255,255)
+
+window = pygame.display.set_mode((500, 500))
 is_paused = False
 volume = mixer.music.get_volume()
 
 def choose_music():
     songs=[i for i in os.listdir(directory)]
-    [print(f'{n+1}: {song}') for n, song in enumerate(songs)]
-    choice = os.path.join(directory, songs[int(input())-1])
-    mixer.music.load(choice)
-    mixer.music.queue(choice)
+    count =0
+    for n, song in enumerate(songs):
+        text = font.render(f'{n+1}: {song}', False, white)
+        count += 30
+        window.blit(text, (0, count))
+    return songs
+
+def play_music(song):
+    mixer.music.load(song)
+    mixer.music.queue(song)
     mixer.music.play()
 
-def pause_music(action):
+def define_choice(songs):
+    keys = pygame.key.get_pressed()
+    for key_constant, pressed in enumerate(keys):
+        if pressed and key_constant-30 < len(songs) :
+            choice = key_constant-30
+            song = os.path.join(directory, songs[choice])
+            play_music(song)
+            break
+
+def pause_music():
     global is_paused
-    if is_paused and action=='p':
+    if is_paused:
         mixer.music.unpause()
         is_paused = False
-    elif not is_paused and action=='p':
+    elif not is_paused:
         mixer.music.pause()
         is_paused=True
 
-def manage_volume(action):
-    if action=='-':
+def manage_volume(event):
+    if event == pygame.K_LEFT:
         mixer.music.set_volume(volume-0.9)
-    if action=='+':
+    elif event == pygame.K_RIGHT:
         mixer.music.set_volume(volume+0.9)
 
-def close(action):
-    if action=='e':
-        mixer.music.stop()
-        return True
+def get_action(event):
+    manage_volume(event)
+    if event == pygame.K_q:
+        playlist(directory)
+    elif event == pygame.K_SPACE:
+        pause_music()
+
+def close(event):
+    if event.type == pygame.QUIT:
+        pygame.quit()
+        sys.exit()
 
 def start():
     while True:
-        action = input()
-        if action=='q': playlist(directory)
-        if action=='c': choose_music()
-        if close(action): break
-        pause_music(action)
-        manage_volume(action)
+        for event in pygame.event.get():
+            close(event)
+            if event.type == pygame.KEYDOWN:
+                get_action(event.key)
+
+        define_choice(choose_music())
+        pygame.display.update()
 
 if '__main__'==__name__:
     start()
--- playlist.py
+++ playlist.py
@@ -1,6 +1,5 @@
 import os, pygame
 from pygame import mixer
-
 songs=[]
 
 def playlist(folder):
@@ -12,9 +11,14 @@ def playlist(folder):
     next_song(folder)
 
 def next_song(folder):
+    from main import get_action, close
+
     while True:
         for event in pygame.event.get():
+            close(event)
+            if event.type == pygame.KEYDOWN:
+                get_action(event.key)
             if event.type == pygame.USEREVENT:
                 if len (songs) > 0:
-                    mixer.music.queue(os.path.join(folder,songs.pop()))
+                    mixer.music.queue(os.path.join(folder, songs.pop()))
                 else: return
